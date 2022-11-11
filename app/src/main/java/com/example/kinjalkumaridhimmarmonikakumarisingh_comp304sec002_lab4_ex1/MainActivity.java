465ae93b3c953d23ea4c260c7df7632faa54397a
Kinjalkumari Kishorbhai Dhimmar <67004684+KIN156@users.noreply.github.com>
2022-11-11 00:46:00 -0500

    initial commit

---
+++ app/src/main/java/com/example/kinjalkumaridhimmarmonikakumarisingh_comp304sec002_lab4_ex1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kinjalkumaridhimmarmonikakumarisingh_comp304sec002_lab4_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
